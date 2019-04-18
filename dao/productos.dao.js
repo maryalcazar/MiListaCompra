11707f26c571a79abc63003b708e0035f066f21b
xSorby <xSorby@github.com>
2019-04-18 09:20:47 +0200

    POST

--- dao/productos.dao.js
+++ dao/productos.dao.js
@@ -1,25 +1,15 @@
-var productos = require('../model/productos');
+var Productos = require('../model/productos');
 
 var ProductosDAO = {};
 
 ProductosDAO.findAll = function () {
-    productos.find().then(producto => {
-        var responseData = "";
-        for (var i = 0; i < producto.length; i++) {
-            var productNombre = producto[i]._doc.nombre;
-            responseData += `<p>${productNombre}</p>`;
-        }
-        return responseData;
-    }).catch(err => {
-        console.error(err);
-    })
+    return Productos.find();
 }
 ProductosDAO.saveOne = function (productName) {
-        var newProduct = new productos();
-        newProduct.nombre = productName;
-        newProduct.save(function (err) {
-            if (err) return handleError(err);
-          });
-          //newProduct.close();
+    var newProduct = new Productos({ nombre: productName });
+    newProduct.save(function (err) {
+        if (err) return handleError(err);
+        console.log('Saved');
+    });
 }
 module.exports = ProductosDAO;
--- main.js
+++ main.js
@@ -14,22 +14,29 @@ var webServer = express();
 webServer.listen(8080);
 webServer.use(express.static(__dirname));
 
-var bodyParser = require('body-parser');
-webServer.use(bodyParser.json());
-webServer.use(bodyParser.urlencoded({ extended: false }));
+//var bodyParser = require('body-parser');
+//webServer.use(bodyParser.json());
+//webServer.use(bodyParser.urlencoded({ extended: false }));
 
 webServer.get('/lista', function (req, res) {
-    var result = ProductosDAO.findAll();
-    res.send(result);
+    ProductosDAO.findAll().then(producto => {
+        var responseData = "";
+        for (var i = 0; i < producto.length; i++) {
+            var productNombre = producto[i]._doc.nombre;
+            responseData += `<p>${productNombre}</p>`;
+        }
+        res.send(responseData);
+    }).catch(err => {
+        console.error(err);
+    });
 });
 
 webServer.post('/addname', function (req, res) {
     var productName = req.body.nombre;
     ProductosDAO.saveOne(productName);
+    res.end();
 });
 
 webServer.get('*', function (req, res) {
     res.redirect('/public');
 });
-
-
